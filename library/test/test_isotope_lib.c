5d347827a24e831bbf2d8797d2ae777ac8ca99a6
jyshin <jiyong.shin@yale.edu>
2017-01-02 13:24:16 -0500

    isotope test updated.

--- library/test/test_isotope_lib.c
+++ library/test/test_isotope_lib.c
@@ -15,6 +15,89 @@
 
 #include "../isotope_lib.h"
 
+static inline off_t get_offset(off_t addr, int block_size) {
+	return addr * block_size;
+}
+
+void test_get_versions(int fd) {
+	unsigned int version;
+	version = get_curr_ver(fd);
+	version = get_oldest_ver(fd);
+}
+
+void test_set_cached(int fd) {
+
+}
+
+void test_subblock_tx(int fd, char *page, int block_size) {
+	begin_tx(fd);
+	mark_accessed(fd, 0, 9, 1, 1);
+	mark_accessed(fd, 1, 9, 0, 1);
+	mark_accessed(fd, 2, 9, 4, 4);
+	mark_accessed(fd, 3, 9, 6, 1);
+	mark_accessed(fd, 4, 9, 7, 1);
+	end_tx(fd);
+}
+
+void test_read_only_tx(int fd, char *page, int block_size) {
+	int i;
+	int addrs_size = 8;
+	off_t addrs[] = {0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7};
+
+	begin_tx(fd);
+
+	for (i = 0; i < addrs_size; i++) {
+		pread(fd, page, block_size, get_offset(addrs[i], block_size));
+		fprintf(stdout, "%c", page[0]);
+	}
+	fprintf(stdout, "\n");
+
+	end_tx(fd);
+}
+
+void test_write_only_tx(int fd, char *page, int block_size) {
+	int i;
+	int addrs_size = 16;
+	off_t addrs[] = {0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7};
+	int ch = 'A';
+
+	begin_tx(fd);
+
+	for (i = 0; i < addrs_size; i++) {
+		page[0] = ch + i;
+		pwrite(fd, page, block_size, get_offset(addrs[i], block_size));
+	}
+
+	end_tx(fd);
+}
+
+void test_read_write_tx(int fd, char *page, int block_size) {
+	int i;
+	int addrs_size = 8;
+	off_t addrs[] = {0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7};
+	int ch = 'a';
+
+	begin_tx(fd);
+
+	for (i = 0; i < addrs_size; i++) {
+		page[0] = ch + i;
+		pwrite(fd, page, block_size, get_offset(addrs[i], block_size));
+	}
+	for (i = 0; i < addrs_size; i++) {
+		pread(fd, page, block_size, get_offset(addrs[i], block_size));
+		fprintf(stdout, "%c", page[0]);
+	}
+	fprintf(stdout, "\n");
+
+	end_tx(fd);
+}
+
+void test_abort_tx(int fd) {
+	begin_tx(fd);
+	abort_tx(fd);
+	end_tx(fd);
+}
+
 int main(int argc, char *argv[])
 {
 	char *page;
@@ -45,43 +128,12 @@ int main(int argc, char *argv[])
 	fd = open("/dev/mapper/dm-tx", O_DIRECT | O_RDWR);
         assert (fd >= 0);
 
-	get_curr_ver(fd);
-	get_oldest_ver(fd);
-
-	begin_tx(fd);
-	printf("Tx begin\n");
-
-	int ch = '1';
-
-	for (i = 0; i < 10; i++) {
-
-		lseek(fd, 0, SEEK_SET);
-		page[0] = (char) (ch+i);
-		printf("write starts at %d with %c\n", 0, page[0]);
-
-		byte_cnt = write(fd, page, block_size);
-		assert (byte_cnt == block_size);
-
-                assert (byte_cnt >= 0);
-		if (byte_cnt == 0) {
-			fprintf(stdout, "End of file\n");
-			break;
-		}
-	}
-	mark_accessed(fd, 5, 9, 1, 1);
-	mark_accessed(fd, 6, 9, 0, 1);
-	mark_accessed(fd, 7, 9, 4, 4);
-	mark_accessed(fd, 8, 9, 6, 1);
-	mark_accessed(fd, 9, 9, 7, 1);
-
-	ch = getchar();
-
-	end_tx(fd);
-
-	lseek(fd, 0, SEEK_SET);
-	byte_cnt = read(fd, page, block_size);
-	assert (byte_cnt >= 0);
-	printf("after tx read %c\n", page[0]);
+	test_get_versions(fd);
+	test_write_only_tx(fd, page, block_size);
+	test_read_only_tx(fd, page, block_size);
+	test_read_write_tx(fd, page, block_size);
+	test_read_only_tx(fd, page, block_size);
+	test_abort_tx(fd);
 
 	close(fd);
 	return 0;
